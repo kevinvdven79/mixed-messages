ac5eee1155fedf4e3936f0ce3900f9f9e2a22102
Kevin van der Ven <44703330+kevinvdven79@users.noreply.github.com>
2022-02-11 11:54:41 +0100

    added random select

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ const mixedMessages = () =>{
     const firstLine = ['Wie is de mol','Trust no one','Zit je te mollen?'];
     const secondLine = ['Mollenstreken','Ik wil een joker', 'Win een vrijstelling'];
     const thirdLine = ['De zwarte vrijstelling', 'Molgeld', 'Kandidaats gedrag'];
+    arrayLength = '';
+    const randomSelect = array =>{
+        arrayLength = length.array;
+        randomLine = Math.floor(Math.random()*arrayLength);
+    }
+
 }

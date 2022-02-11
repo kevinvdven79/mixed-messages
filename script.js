2424790e2556ca57901d763891b29e7e2f891e36
Kevin van der Ven <44703330+kevinvdven79@users.noreply.github.com>
2022-02-11 13:40:05 +0100

    Edited random select, added variables for each random line

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const mixedMessages = () =>{
     const firstLine = ['Wie is de mol','Trust no one','Zit je te mollen?'];
     const secondLine = ['Mollenstreken','Ik wil een joker', 'Win een vrijstelling'];
     const thirdLine = ['De zwarte vrijstelling', 'Molgeld', 'Kandidaats gedrag'];
-    arrayLength = '';
+    let arrayLength = '';
     const randomSelect = array =>{
         arrayLength = length.array;
-        randomLine = Math.floor(Math.random()*arrayLength);
+        let randomLine = array[Math.floor(Math.random()*arrayLength)];
+        return randomLine;
     }
-
+    const randomOne = randomSelect(firstLine);
+    const randomTwo = randomSelect(secondLine);
+    const randomThree = randomSelect(thirdLine);
 }
